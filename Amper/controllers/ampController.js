00307e7473ef9c8bc80cb18d0125f01688fe25b0
Satish Yadav <monstersatish@LIVE.COM>
2017-08-21 01:13:33 +0530

    Add project files.

---
+++ Amper/controllers/ampController.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+var amp = require('ampl');
+exports.htmlToAmp = function (req, res) {
+    var html = req.body.md;
+    var css = '';
+    amp.parse(html, css, function (ampHtml) {
+        console.log(ampHtml);
+        res.send(ampHtml);
+    });
+
+}
+
+exports.noGet = function (req, res) {
+    res.send('use POST instead.');
+}
---
+++ Amper/routes/amp.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var amp = require('../controllers/ampController');
+
+/* GET users listing. */
+router.get('/', amp.noGet).post("/", amp.htmlToAmp);
+
+module.exports = router;
